7f6b2d476915c86b84cf6dfb992ee8206adcb0da
AhmedOmara14 <ahmedomaea53@gmail.com>
2022-09-13 22:00:47 +0200

    Constrain Layout

--- app/src/main/java/com/atw/jetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/atw/jetpackcompose/presentation/MainActivity.kt
@@ -42,7 +42,6 @@ class MainActivity : ComponentActivity() {
                     SetDataInGrid()
                 }
             }**/
-
             //Constrain Layout
            /** ConstraintLayout()**/
         }
